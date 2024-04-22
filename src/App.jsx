9efa336fcbc96fbb2dc9f44e45c6e4de0d9772de
giftedpromise <giftedpromise1992@gmail.com>
2024-04-22 00:17:05 -1200

    update React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const welcome = "Welcome to 30 Days Of React";
+  const title = "Getting Started React";
+  const header = (
+    <header>
+      <div className="header-wrapper">
+        <h1>Welcome to 30 Days Of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>JavaScript Library</h3>
+        <p>Instructor: Asabeneh Yetayeh</p>
+        <small>Date: Oct 1, 2020</small>
+      </div>
+    </header>
+  );
+
+  // JSX element, main
+  const main = (
+    <main>
+      <div className="main-wrapper">
+        <p>
+          Prerequisite to get started{" "}
+          <strong>
+            <em>react.js</em>
+          </strong>
+          :
+        </p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li> JavaScript</li>
+        </ul>
+      </div>
+    </main>
+  );
+
+  // JSX element, footer
+  const footer = (
+    <footer>
+      <div className="footer-wrapper">
+        <p>Copyright 2020</p>
+      </div>
+    </footer>
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {header}
+      {main}
+      {footer}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
